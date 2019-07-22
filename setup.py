57ff7fa73197eb7b48e2e360eefa4cc2d7f35af0
Michael Silver <rush340@gmail.com>
2019-07-22 11:31:35 -0700

    python repo setup

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+
+setup(
+    name="cosmobot_deep_learning",
+    version="0.0.1",
+    author="Osmo Systems",
+    author_email="dev@osmobot.com",
+    description="Cosmobot deep learning models",
+    url="https://www.github.com/osmosystems/cosmobot-deep-learning.git",
+    packages=find_packages(),
+    include_package_data=True,
+)
