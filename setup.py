7044ed39463ebd8d7bf3ae134837d20314407ea5
Evan Simpson <EvanSimpson@users.noreply.github.com>
2019-08-01 17:17:58 -0400

    Add missing pieces to run models in Jupyter Notebooks (#5)
    * Add missing dependencies
    
    * Add module entrypoint to models
    
    * Add comment to tensorflow version

--- setup.py
+++ setup.py
@@ -11,13 +11,20 @@ setup(
     packages=find_packages(),
     # fmt: off
     install_requires=[
+        "keras",
+        "keras_resnet",
+        "numpy",
         "opencv-python",
         # Pandas 0.25 breaks tqdm (for now)
         # We can unpin when https://github.com/tqdm/tqdm/issues/780 is fixed
         "pandas <=0.24.2",
         "picamraw",
         "scipy",
+        # Newer versions of tensorflow have memory issues on our graphics cards
+        # https://github.com/tensorflow/tensorflow/issues/24496#issuecomment-456243093
+        "tensorflow-gpu==1.12.0",
         "tqdm",
+        "wandb",
     ],
     # fmt: on
     include_package_data=True,
