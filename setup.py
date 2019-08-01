c88b186d194e28170ee0366a19d1f2d57bcba153
EvanSimpson <evan@osmobot.com>
2019-08-01 15:35:32 -0400

    Add missing dependencies

--- setup.py
+++ setup.py
@@ -11,13 +11,18 @@ setup(
     packages=find_packages(),
     # fmt: off
     install_requires=[
+        "keras",
+        "keras_resnet",
+        "numpy",
         "opencv-python",
         # Pandas 0.25 breaks tqdm (for now)
         # We can unpin when https://github.com/tqdm/tqdm/issues/780 is fixed
         "pandas <=0.24.2",
         "picamraw",
         "scipy",
+        "tensorflow-gpu==1.12.0",
         "tqdm",
+        "wandb",
     ],
     # fmt: on
     include_package_data=True,
