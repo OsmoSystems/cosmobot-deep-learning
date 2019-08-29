49c5d01fbf1119fe2a952061d9b2f1726d0d268a
EvanSimpson <evan@osmobot.com>
2019-08-29 15:35:15 -0400

    Refactor ROI dataset preparation

--- cosmobot_deep_learning/prepare_dataset.py
+++ cosmobot_deep_learning/prepare_dataset.py
@@ -120,6 +120,27 @@ def prepare_dataset_image_and_numeric(raw_dataset: pd.DataFrame, hyperparameters
     )
 
 
+def _extract_ROI_and_numeric_features_and_labels(samples, hyperparameters):
+    numeric_input_columns = hyperparameters["numeric_input_columns"]
+    label_column = hyperparameters["label_column"]
+    label_scale_factor_mmhg = hyperparameters["label_scale_factor_mmhg"]
+    image_size = hyperparameters["image_size"]
+    input_ROI_names = hyperparameters["input_ROI_names"]
+
+    # Use ast to safely eval ROI definitions to dict
+    ROI_definitions = samples["ROI definitions"].apply(ast.literal_eval)
+
+    x_numeric = extract_inputs(samples, numeric_input_columns)
+    x_crops = open_and_preprocess_image_ROIs(
+        list(zip(samples["local_filepath"], ROI_definitions)),
+        input_ROI_names,
+        image_size,
+    )
+    y = extract_labels(samples, label_column, label_scale_factor_mmhg)
+
+    return ([x_numeric] + list(x_crops), y)
+
+
 def prepare_dataset_ROIs_and_numeric(raw_dataset: pd.DataFrame, hyperparameters):
     """ Transform a dataset CSV into the appropriate inputs and labels for training and
     validating a model, for a model that uses multiple ROI-crop input images as well as numeric inputs
@@ -135,41 +156,17 @@ def prepare_dataset_ROIs_and_numeric(raw_dataset: pd.DataFrame, hyperparameters)
         Returns:
             A 4-tuple containing (x_train, y_train, x_test, y_test) data sets.
     """
-    numeric_input_columns = hyperparameters["numeric_input_columns"]
-    label_column = hyperparameters["label_column"]
-    label_scale_factor_mmhg = hyperparameters["label_scale_factor_mmhg"]
-    image_size = hyperparameters["image_size"]
     training_set_column = hyperparameters["training_set_column"]
     dev_set_column = hyperparameters["dev_set_column"]
-    input_ROI_names = hyperparameters["input_ROI_names"]
 
     train_samples = raw_dataset[raw_dataset[training_set_column]]
     test_samples = raw_dataset[raw_dataset[dev_set_column]]
 
-    # Use ast to safely eval ROI definitions to dict
-    train_ROI_definitions = train_samples["ROI definitions"].apply(ast.literal_eval)
-
-    x_train_numeric = extract_inputs(train_samples, numeric_input_columns)
-    x_train_crops = open_and_preprocess_image_ROIs(
-        list(zip(train_samples["local_filepath"], train_ROI_definitions)),
-        input_ROI_names,
-        image_size,
+    x_train, y_train = _extract_ROI_and_numeric_features_and_labels(
+        train_samples, hyperparameters
     )
-    y_train = extract_labels(train_samples, label_column, label_scale_factor_mmhg)
-
-    test_ROI_definitions = test_samples["ROI definitions"].apply(ast.literal_eval)
-
-    x_test_numeric = extract_inputs(test_samples, numeric_input_columns)
-    x_test_crops = open_and_preprocess_image_ROIs(
-        list(zip(test_samples["local_filepath"], test_ROI_definitions)),
-        input_ROI_names,
-        image_size,
+    x_test, y_test = _extract_ROI_and_numeric_features_and_labels(
+        test_samples, hyperparameters
     )
-    y_test = extract_labels(test_samples, label_column, label_scale_factor_mmhg)
 
-    return (
-        [x_train_numeric] + list(x_train_crops),
-        y_train,
-        [x_test_numeric] + list(x_test_crops),
-        y_test,
-    )
+    return (x_train, y_train, x_test, y_test)
