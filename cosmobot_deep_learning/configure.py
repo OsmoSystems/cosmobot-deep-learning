f14d3cbd5d1e1e6930e9725c39c1c16e7d2b8792
Michael Silver <rush340@gmail.com>
2019-09-12 16:57:06 -0700

    add sweeps for DO setpoint variation

--- cosmobot_deep_learning/configure.py
+++ cosmobot_deep_learning/configure.py
@@ -93,6 +93,9 @@ def parse_model_run_args(
     )
     arg_parser.add_argument("--learning-rate", type=float)
 
+    # not used, just included to be able to do a grid hyperparameter sweep over it
+    arg_parser.add_argument("--repeat", type=int)
+
     arg_namespace = arg_parser.parse_args(args)
     return arg_namespace
 
